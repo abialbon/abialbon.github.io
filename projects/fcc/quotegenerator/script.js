24f648c3a8616b5fef0d6558e65b69704a37e0d8
Abialbon Paul <abialbon@abialbons-air.nevis.mua.local>
2017-05-11 15:22:15 -0400

    FCC Quote Generator Project

---
+++ projects/fcc/quotegenerator/script.js
@@ -0,0 +1,55 @@
+var getQuotes = function () {
+  $.ajax({
+    type: "POST",
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=10",
+    headers: {
+      'X-Mashape-Key': 'X7E4CWYMQ6msh7t9bo6bqBiyrmOop1qbIJTjsnlmSQtfy9M1kq'
+    },
+    success: function (data) {
+      app.quotes = app.quotes.concat(JSON.parse(data));
+      console.log(app.quotes)
+    }
+  });
+}
+
+
+$('#close').on('click', function () {
+  $('.slide').toggle();
+})
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    text: "Hello from Vue!",
+    quotes: [{
+      "quote": 'Coding is my passion! Click on "New Quote" to start the Random Quote Generator',
+      "author": "Abialbon Paul"
+    }],
+    qindex: 0,
+    twQuote: ""
+  },
+  methods: {
+    getQuotes: function () {
+      getQuotes();
+    },
+    nextQuote: function () {
+      if (this.qindex == this.quotes.length - 2) {
+        this.getQuotes();
+        this.qindex++;
+      } else {
+        this.qindex++;
+      }
+      console.log(this.qindex)
+    },
+    tweet: function () {
+      window.open(this.twQuote)
+    }
+  },
+  created: function () {
+    getQuotes();
+  },
+  updated: function () {
+    app.twQuote = 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURI(app.quotes[app.qindex].quote) + ' - ' + encodeURI(app.quotes[app.qindex].author);
+  }
+});

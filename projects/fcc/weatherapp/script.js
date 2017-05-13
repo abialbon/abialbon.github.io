063b181d84cd91c1b74463a3e1653281576eda1b
Abialbon Paul <abialbon@Abialbons-MacBook-Air.local>
2017-05-13 14:08:05 -0400

    Crossorigin.me

--- projects/fcc/weatherapp/script.js
+++ projects/fcc/weatherapp/script.js
@@ -27,48 +27,58 @@ $(document).ready(function(){
         navigator.geolocation.getCurrentPosition(function(x) {
             var long = x.coords.longitude;
             var lat = x.coords.latitude;
-            var urlToOpen = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + '85ad46f83d5801cdebcbc16ab42983e7'
+            var urlToOpen = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + '85ad46f83d5801cdebcbc16ab42983e7'
+            $.get(urlToOpen, function(data) {
+          // Populating
+          weatherData = data;
+          location = weatherData.name + ", " + weatherData.sys.country;
+          icon = "icons/" + weatherData.weather[0].icon + ".svg";
+          description = weatherData.weather[0].description;
+          sunrise = (function() {
+            var timestamp = new Date(null);
+            timestamp.setSeconds(weatherData.sys.sunrise);
+            return timestamp;
+          })();
+          sunset = (function() {
+            var timestamp = new Date(null);
+            timestamp.setSeconds(weatherData.sys.sunset);
+            return timestamp;
+          })();
+          // Updating
+          updateHTML("#location", location);
+          updateHTML("#temperature", toCelsius(weatherData.main.temp));
+          updateHTML("#description", description);
+          updateHTML(
+            "#sunrise",
+            "Sunrise: " + sunrise.toLocaleString().substr(11)
+          );
+          updateHTML(
+            "#sunset",
+            "Sunset: " + sunset.toLocaleString().substr(11)
+          );
+          updateIcon(icon);
+          // Remove preloader
+          $(".spinner").hide();
+          $(".wrapper").fadeIn();
+        }
+      );
+    });
+  } else {
+    $("#preload-text").html(
+      "Oops, your location is unavailable and I can't handle this!"
+    );
+    $(".preload").hide();
+  }
 
-            $.ajax({
-                url: urlToOpen,
-                method: 'GET',
-                header: { 'Content-Type': 'application/xml',
-                'X-paul': 'paul' },
-                dataType: 'jsonp',
-                cache: true,
-                success: function(data){
-                    // Populating
-                    weatherData = data;
-                    location = weatherData.name + ', ' + weatherData.sys.country;
-                    icon = 'icons/' + weatherData.weather[0].icon + '.svg';
-                    description = weatherData.weather[0].description;
-                    sunrise = (function() {
-                        var timestamp = new Date(null);
-                        timestamp.setSeconds(weatherData.sys.sunrise);
-                        return timestamp;
-                    })();
-                    sunset = (function() {
-                        var timestamp = new Date(null);
-                        timestamp.setSeconds(weatherData.sys.sunset);
-                        return timestamp;
-                    })();
-                    // Updating
-                    updateHTML('#location', location);
-                    updateHTML('#temperature', toCelsius(weatherData.main.temp))
-                    updateHTML('#description', description)
-                    updateHTML('#sunrise', 'Sunrise: '+ sunrise.toLocaleString().substr(11))
-                    updateHTML('#sunset', 'Sunset: '+ sunset.toLocaleString().substr(11))
-                    updateIcon(icon)
-                    // Remove preloader
-                    $('.spinner').hide();
-                    $('.wrapper').fadeIn();
-                }
-            });
-        });
+  // Temp conversions
+  $("input[name=temp]").on("click", function(val) {
+    console.log(val.target.value);
+    if (val.target.value == "fah") {
+      updateHTML("#temperature", toFahrenheit(weatherData.main.temp));
     } else {
-        $('#preload-text').html("Oops, your location is unavailable and I can't handle this!");
-        $('.preload').hide();
+      updateHTML("#temperature", toCelsius(weatherData.main.temp));
     }
+  });
 
     // Temp conversions
     $('input[name=temp]').on('click', function(val){ 
